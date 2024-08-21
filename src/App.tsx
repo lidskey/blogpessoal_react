0de820d13e41aa3b795969139145085e7dd3ce40
Liara Cristina <lidskey@hotmail.com>
2024-08-21 12:10:51 -0300

    feat: componente home - css

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Home from "./pages/home/home"
+
+
+function App() {
+  //código typescript
+  
+ 
+  return (
+    //o return tem oq vai aparecer na tela
+    //codigo tsx equivalente a html e css
+    <>
+      <Home />
+      
+    </>
+
+
+  )
+}
+
+export default App
